da6722e5b9f70ce8af0b23ddcd5dbffc29fc059d
lidehua <lidehua@ececloud.cn>
2020-05-29 17:53:23 +0800

    admin
    Signed-off-by: lidehua <lidehua@ececloud.cn>

--- service/admin.go
+++ service/admin.go
@@ -6,6 +6,15 @@ import (
 	"my/model"
 )
 
+func Admin(adminId int) (*model.Admin, error) {
+	var admin *model.Admin
+	error := global.GVA_DB.Find(&admin, adminId).Error
+	if error != nil && error != gorm.ErrRecordNotFound {
+		return nil, error
+	}
+	return admin, nil
+}
+
 func AdminList(pageNum int, pageSize int) ([]*model.Admin, error) {
 	var admins []*model.Admin
 	error := global.GVA_DB.Offset((pageNum - 1) * pageSize).Limit(pageSize).Find(&admins).Error
@@ -27,7 +36,7 @@ func AdminByLoginName(LoginName string) (*model.Admin, error) {
 func UpdateAdmin(model.Admin) (*model.Admin, error) {
 	var admin *model.Admin
 	error := global.GVA_DB.Update(&admin).Error
-	if error != nil && error != gorm.ErrRecordNotFound {
+	if error != nil {
 		return nil, error
 	}
 	return admin, nil
@@ -35,8 +44,19 @@ func UpdateAdmin(model.Admin) (*model.Admin, error) {
 
 func AddAdmin(admin model.Admin) (*model.Admin, error) {
 	error := global.GVA_DB.Save(&admin).Error
-	if error != nil && error != gorm.ErrRecordNotFound {
+	if error != nil {
 		return nil, error
 	}
 	return &admin, nil
+}
+
+func DeleteAdmin(adminId int) error {
+	var admin = new(model.Admin)
+	admin.Delete = true
+	admin.Id = adminId
+	error := global.GVA_DB.Update(&admin).Error
+	if error != nil {
+		return error
+	}
+	return nil
 }
